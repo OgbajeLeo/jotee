192284cff3a707dce7b98ec509f81c2f91db57d1
Ogbajeleo <leonardaromeh@gmail.com>
2025-09-25 01:44:57 +0100

    Updates

--- src/pages/landingPage/sections/HeroSection.tsx
+++ src/pages/landingPage/sections/HeroSection.tsx
@@ -40,11 +40,11 @@ const HeroSection = () => {
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-gradient-to-br from-gray-50 via-white to-gray-100">
       {/* Background Image */}
       <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        className="absolute inset-0 bg-cover bg-black bg-center bg-no-repeat"
         style={{
           backgroundImage:
-            "url('https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80')",
-          opacity: 0.3,
+            "url('https://lh3.googleusercontent.com/p/AF1QipPA4gNUC-RYTFT7NLNrt0TGuhVbk1HXtqu8nN7_=s1360-w1360-h1020-rw')",
+          opacity: 0.2,
         }}
       />
 
